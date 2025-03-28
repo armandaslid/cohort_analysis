dd37ef2862ae54272844db437731f87472dc5956
Armandas Lidžius <armandaslid@gmail.com>
2025-03-28 16:40:37 +0100

    Create retention_rate_by_cohort.sql
    Created Retention Rate By Cohort SQL file

---
+++ SQL_Files/retention_rate_by_cohort.sql
@@ -0,0 +1,31 @@
+WITH months AS (
+  SELECT
+    customer_id,
+    DATE(DATE_TRUNC('MONTH', first_purchase)) AS first_month,
+    DATE(DATE_TRUNC('MONTH', second_purchase)) AS second_month
+  FROM 
+    cohort_analysis
+),
+
+tt AS (
+  SELECT
+    customer_id,
+    first_month AS month_group,
+    MONTHS_BETWEEN(second_month, first_month) AS months_between
+  FROM 
+    months
+)
+
+SELECT 
+  month_group,
+  COUNT(DISTINCT customer_id) AS total_customers,
+  ROUND(COUNT(DISTINCT CASE WHEN months_between <= 1 THEN customer_id ELSE NULL END) / COUNT(DISTINCT customer_id), 2) AS retention_rate_1m,
+  ROUND(COUNT(DISTINCT CASE WHEN months_between <= 2 THEN customer_id ELSE NULL END) / COUNT(DISTINCT customer_id), 2) AS retention_rate_2m,
+  ROUND(COUNT(DISTINCT CASE WHEN months_between <= 3 THEN customer_id ELSE NULL END) / COUNT(DISTINCT customer_id), 2) AS retention_rate_3m
+FROM 
+  tt
+GROUP BY 
+  month_group
+ORDER BY 
+  month_group
+;

c819be7365b8840b0fc30ab70d30c6de5a932c20
Armandas Lidžius <armandaslid@gmail.com>
2025-03-28 18:16:07 +0100

    Update cohort_size_by_month.sql
    Added comments

--- SQL_Files/cohort_size_by_month.sql
+++ SQL_Files/cohort_size_by_month.sql
@@ -1,15 +1,21 @@
-WITH first_purchase AS (
+/*
+This query calculates the cohort size for customers based on their first purchase month.
+It groups customers into cohorts by the month of their first purchase and counts the number
+of customers in each cohort.
+*/
+
+WITH first_purchase AS (        -- CTE for first purchase month for each customer
   SELECT
-    customer_id,
-    DATE(MIN(DATE_TRUNC('MONTH', order_date))) AS first_purchase_date
+    customer_id
+    ,DATE(MIN(DATE_TRUNC('MONTH', order_date))) AS first_purchase_date
   FROM
     bigquery_db_cohort_db.ecom_orders
   GROUP BY customer_id
 )
 
-SELECT
-  first_purchase_date AS cohort_month,
-  COUNT(customer_id) AS cohort_size
+SELECT                          -- Counted the number of customers in each cohort
+  first_purchase_date AS cohort_month
+  ,COUNT(customer_id) AS cohort_size
 FROM 
   first_purchase
 GROUP BY 

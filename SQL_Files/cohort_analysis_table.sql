4e18bed5ad47247ed44526c5190e6bbfb5d7c388
Armandas Lidžius <armandaslid@gmail.com>
2025-03-28 16:37:20 +0100

    Create cohort_analysis_table.sql
    Created a Cohort Analysis Table SQL file

---
+++ SQL_Files/cohort_analysis_table.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE TABLE cohort_analysis AS
+
+WITH first_purchase AS (
+SELECT
+  customer_id
+  ,MIN(order_date) AS first_purchase
+FROM
+  ecom_orders_csv
+GROUP BY 1
+),
+
+second_purchase AS (
+SELECT
+  customer_id
+  ,order_date AS second_purchase
+FROM (SELECT
+        customer_id
+        ,order_date
+        ,ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS purchase_num
+      FROM
+        ecom_orders_csv)
+WHERE purchase_num = 2
+)
+
+SELECT 
+  fp.customer_id
+  ,fp.first_purchase
+  ,sp.second_purchase
+  ,DATE_DIFF(sp.second_purchase, fp.first_purchase) AS days_between_1_and_2
+FROM first_purchase AS fp
+LEFT JOIN second_purchase AS sp
+ON fp.customer_id = sp.customer_id
